ac1076724a6468afc2c1738f7ad272bdb9990020
melessevie <eviemelesse@seattleacademy.org>
2025-03-29 02:54:57 +0000

    project2.py

---
+++ Projects/project2.py
@@ -0,0 +1,44 @@
+# Beginning: Create variables 
+Chocolate_points = 0
+Vanilla_points = 0
+
+
+answer = input ("Are you A) a Chocolate ice cream person, or B) a Vanilla ice cream person?")
+if answer == "A":
+      Chocolate_points += 1
+elif answer == "B" :
+       Vanilla_points   += 1
+
+# Medium: As questions
+answer = input ("are you A) a quite person, or B) a loud person?") 
+if answer == "A":
+      Chocolate_points += 1
+elif answer == "B" :
+       Vanilla_points   += 1
+
+
+answer = input ("are you A) a confident person, or B) a shy person?") 
+if answer == "A":
+      Chocolate_points += 1
+elif answer == "B" :  
+
+
+answer = input ("are you A) a Vanilla perfume person, or B) a strong sexy perfume person?")
+if answer == "A":
+      Chocolate_points += 1
+elif answer == "B" :
+
+
+answer = input ("are you A) a Introverted person, or B) a Extroverted person?")
+if answer == "A":
+      Chocolate_points += 1
+elif answer == "B" :
+
+
+# End: Determine results
+if Vanilla_points > Chocolate_points:
+      print("you are a Vanilla person")
+elif Chocolate_points > Vanilla_points:
+      print("you are a Chocolate person")
+elif Vanilla_points == Chocolate_points:
+      print("you are both Vanilla and Chocolate at the same time")
---
+++ grandma.py
@@ -0,0 +1,8 @@
+while True:
+    word= input("what do you think grandma likes?")
+    if "two of the same letter" in word:
+        print(f"Grandma doesn't like {word}")
+    else:
+        print (f"Grandma likes {word}")
+        print("")
+        
