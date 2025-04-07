839a9b214cd0f7776149a507f2645f3b542d273d
melessevie <eviemelesse@seattleacademy.org>
2025-04-07 19:05:45 +0000

    Evies project

---
+++ Projects/code_art 3.py
@@ -0,0 +1,10 @@
+import turtle # the arrow
+
+t= turtle.Turtle ()
+
+t.color("brown") # the color of the shape
+turtle.Screen().bgcolor("orange")
+for i in range (100) : # the amount of times it will repeat 
+    t.forward (45+69)
+    t.left(67)
+turtle.exitonclick ()
--- Projects/project2.py
+++ Projects/project2.py
@@ -23,21 +23,21 @@ if answer == "A":
 elif answer == "B" :  
 
 
-answer = input ("are you A) a Vanilla perfume person, or B) a strong sexy perfume person?")
+      answer = input ("are you A) a Vanilla perfume person, or B) a strong sexy perfume person?")
 if answer == "A":
       Chocolate_points += 1
 elif answer == "B" :
 
 
-answer = input ("are you A) a Introverted person, or B) a Extroverted person?")
+      answer = input ("are you A) a Introverted person, or B) a Extroverted person?")
 if answer == "A":
       Chocolate_points += 1
 elif answer == "B" :
 
 
 # End: Determine results
-if Vanilla_points > Chocolate_points:
-      print("you are a Vanilla person")
+      if Vanilla_points > Chocolate_points:
+            print("you are a Vanilla person")
 elif Chocolate_points > Vanilla_points:
       print("you are a Chocolate person")
 elif Vanilla_points == Chocolate_points:
